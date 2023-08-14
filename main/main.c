18a6758826531494af664a19f4d6f5f973037484
casdata <casdata@gmail.com>
2023-08-14 14:03:58 -0500

    - Added thermistor reading - Fully implemented LCD 1601 driver.

--- main/main.c
+++ main/main.c
@@ -8,7 +8,7 @@
 #include "driver/i2c.h"
 #include "esp_log.h"
 #include "extend.h"
-
+#include "math.h"
 
 
 
@@ -22,6 +22,33 @@ bool cupSensorSw = false;
 
 uint8_t *inputIO_Buff;
 uint8_t *outputIO_Buff;
+
+/*
+float thermA =  0.6989527046e-3;              //Calculated Stainhart-Hart model coefficients
+float thermB =  2.948656661e-4;
+float thermC =  -1.381899722e-7;
+
+ * 35875= 2.18  (0)
+ * 12000= 25    (25)
+ * 2900 = 62.45 (50)
+ * 1475 = 84.41 (85)
+ * 1260 = 90    (90)
+ * 963 = 100    (100)
+ */
+
+
+float thermA =  0.9430468202e-3;              //Calculated Stainhart-Hart model coefficients
+float thermB =  2.480482099e-4;
+float thermC =  1.009595903e-7;
+
+/*
+ * 35875= 0     (0)
+ * 12000= 24.78 (25)
+ * 2900 = 63.35 (50)
+ * 1475 = 85    (85)
+ * 1260 = 90.41 (90)
+ * 963 =  100   (100)
+ */
 
 
 QueueHandle_t xQueueIntB;
@@ -42,7 +69,7 @@ void setBit2Word(uint16_t *word, int bitValue, int8_t position){
     *word += (bitValue << position);
 }
 
-int getBit2Word(const uint16_t *word, int8_t position){
+int getBit2Word(const uint8_t *word, int8_t position){
     int bitValue = *word >> position;
 
     bitValue &= 1;
@@ -129,7 +156,19 @@ void readTemp(uint16_t *thermTemp){
     uint16_t adcValue;
 
     if(readMCP3421_ADC(&adcValue)){
-        ESP_LOGI(TEMP_TASK_TAG, "Raw temp: %d", adcValue);
+
+        float thermistorVoltage = adcValue * 0.001f;
+
+        uint16_t thermistorValue = (thermistorVoltage * 12000)/(3.3f - thermistorVoltage);
+
+        //ESP_LOGI(TEMP_TASK_TAG, "Raw temp: %d - %.4f - %d", adcValue, thermistorVoltage, thermistorValue); //1649
+
+        float temp = log(thermistorValue);
+        temp = (1.0f / (thermA + (thermB * temp + (thermC * temp * temp * temp))));
+        temp -= 273.15f;
+
+        //ESP_LOGW(TEMP_TASK_TAG, "t= %.2f", temp);
+
     }
 
 }
@@ -339,11 +378,162 @@ void sendData2LCD(const uint8_t dataByte, const bool rW, const bool rs){
 
 }
 
+void clearLcdScreen(){
+    sendData2LCD(0b00000001, 0, 0);
+}
 
-void write2LCD(){
+void setLcdCursor2Home(){
+    sendData2LCD(0b00000010, 0, 0);
+}
+
+/*  setLcdEntryMode
+ * incrementMode:   Increment / decrement of DDRAM address
+ *                  true : shift display to left
+ *                  fales: shift display to right
+ * shift:           Enables the shift function of the display
+ */
+
+void setLcdEntryMode(const bool incrementMode, const bool shift){
+    uint8_t dataByte = 0b00000100;
+
+    uint8_t dataBit = (uint8_t)incrementMode;
+    dataBit <<= 1;
+    dataByte += dataBit;
+
+    dataBit = (uint8_t)shift;
+    dataByte += dataBit;
+
+    sendData2LCD(dataByte, 0, 0);
+
+}
+
+/*  setLcdOptions
+ * displayOn:   true = display on
+ *              false = dispaly off
+ * cursorOn:    true = cursor on
+ *              fales = cursor off
+ * cursorBlink: true = on
+ *              false = off
+ */
+
+void setLcdOptions(const bool displayOn, const bool cursorOn, const bool cursorBlink) {
+    uint8_t dataByte = 0b00001000;
+
+    uint8_t dataBit = (uint8_t)displayOn;
+    dataBit <<= 2;
+    dataByte += dataBit;
+
+    dataBit = (uint8_t)cursorOn;
+    dataBit <<= 1;
+    dataByte += dataBit;
+
+    dataBit = (uint8_t)cursorBlink;
+    dataByte += dataBit;
+
+    sendData2LCD(dataByte, 0, 0);
+}
+
+/*  setLcdCursorOrShift
+ *  displayShift (S/C):     true = display shift
+ *                          false = cursor
+ *  rightShift (R/L):       true = right shift
+ *                          false = left shift
+ *
+ *  S/C     |   R/L   |
+ *  false       false   Shift cursor to left
+ *  false       true    Shift cursor to the right
+ *  true        false   Shift display to the left. Cursor follows the display shift
+ *  true        true    Shift display to the right. Cursor follows the display shift
+ */
+
+void setLcdCursorOrShift(const bool displayShift, const bool rightShift){
+    uint8_t dataByte = 0b00010000;
+
+    uint8_t dataBit = (uint8_t)displayShift;
+    dataBit <<= 3;
+    dataByte += dataBit;
+
+    dataBit = (uint8_t)rightShift;
+    dataBit <<= 2;
+    dataByte += dataBit;
+
+    sendData2LCD(dataByte, 0, 0);
+
+}
+
+/*  setLcdFunction
+ *  bitDL:      Interface data lenght control bit
+ *              true = 8-bit bus mode
+ *              false = 4-bit bus mode
+ *  bitN:       Display line number control bit
+ *              true = 2-line display mode
+ *              false = 1-line display mode
+ *  bitF:       Display font type control bit
+ *              true = 5x11 dots format
+ *              false = 5x8 dots format
+ */
+
+void setLcdFunction(const bool bitDL, const bool bitN, const bool bitF){
+    uint8_t dataByte = 0b00100000;
+
+    uint8_t dataBit = (uint8_t)bitDL;
+    dataBit <<= 4;
+    dataByte += dataBit;
+
+    dataBit = (uint8_t)bitN;
+    dataBit <<= 3;
+    dataByte += dataBit;
+
+    dataBit = (uint8_t)bitF;
+    dataBit <<= 2;
+    dataByte += dataBit;
+
+    sendData2LCD(dataByte, 0, 0);
+}
+
+/*  setLcdCGRAM_addr
+ *  addr:   CGRAM address to AC
+ *          CGRAM adderss is from 0x0 to 0x7
+ */
+
+void setLcdCGRAM_addr(uint8_t addr){
+    uint8_t dataByte = 0b01000000;
+
+    addr &= 0x3f;
+    dataByte += addr;
+
+    sendData2LCD(dataByte, 0, 0);
+}
+
+/*  setLcdDDRAM_addr
+ *  addr:   DDRAM address to AC
+ *          DDRAM address is from 0x0 to 0x27
+ */
+void setLcdDDRAM_addr(uint8_t addr){
+    uint8_t dataByte = 0b10000000;
+
+    addr &= 0x7f;
+    dataByte += addr;
+
+    sendData2LCD(dataByte, 0, 0);
+}
 
 
+void write2LCD(const char *dataStr, const uint8_t dataStrLength){
 
+    for(size_t i = 0; i < dataStrLength; i++){
+
+        sendData2LCD(*(dataStr + i), 0, 1);
+
+        vTaskDelay(pdMS_TO_TICKS(5));
+    }
+
+    //sendData2LCD(0b01010111, 0, 1);             //Write 'W'
+
+}
+
+void setLcdPos(const uint8_t x){
+    setLcdDDRAM_addr(x);
 }
 
 
@@ -417,7 +607,6 @@ void updateUI(uint16_t *uiSwitches){
     ESP_LOGV("MAIN_TASK", "verbose");
     */
 
-    write2LCD();
                                                             
                                                             
 }
@@ -510,22 +699,58 @@ static void mainTask(void* pvParameters){
 
     uint16_t uiSwitches = 0;
 
+    setLcdFunction(true, false, false);
 
-    sendData2LCD(0b00110000, 0, 0);             //Function set: 8bit-length, one-line, 5x8 dots
+    vTaskDelay(pdMS_TO_TICKS(200));
+
+    setLcdOptions(true, false, false);
 
     vTaskDelay(pdMS_TO_TICKS(200));
 
-    sendData2LCD(0b00001110, 0, 0);             //DISPLAY on, cursor on
+    setLcdEntryMode(true, false);
 
     vTaskDelay(pdMS_TO_TICKS(200));
-   
-    sendData2LCD(0b00000110, 0, 0);             //ENTRY MODE display doesnt shifts - increment
-    
-    vTaskDelay(pdMS_TO_TICKS(200));
 
-    sendData2LCD(0b01010111, 0, 1);             //Write 'W'
+    clearLcdScreen();
 
     vTaskDelay(pdMS_TO_TICKS(200));
+
+    write2LCD("Open Colibri V1.00 2023", 23);
+
+    for(size_t i = 0; i < 7; i++) {
+
+        vTaskDelay(pdMS_TO_TICKS(1000));
+
+        setLcdCursorOrShift(true, false);
+    }
+
+    vTaskDelay(pdMS_TO_TICKS(1000));
+    setLcdCursor2Home();
+
+
+    /*
+    setLcdPos(2);
+    write2LCD("rgio", 4);
+
+    setLcdPos(0);
+    write2LCD("Se", 2);
+    */
+
+    /*
+    sendData2LCD(0b00100001, 0, 1);             //Write 'W'
+
+    vTaskDelay(pdMS_TO_TICKS(200));
+
+    sendData2LCD(0b10110001, 0, 1);             //Write 'W'
+
+    vTaskDelay(pdMS_TO_TICKS(200));
+
+    sendData2LCD(0b11111111, 0, 1);             //Write 'W'
+
+    vTaskDelay(pdMS_TO_TICKS(200));
+
+    sendData2LCD(0b01111110, 0, 1);             //Write 'W'
+    */
     
 
      //sendData2LCD(0, 0, 0);
